49e3274a4875144d3fcdf5e07ea32ccc2b268540
Vito <vitohartanto@mail.ugm.ac.id>
2024-12-22 20:19:51 +0700

    fix

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -10,7 +10,7 @@ const routes = (handler) => [
   {
     method: 'GET',
     path: '/threads/{threadId}',
-    handler: (request, h) => handler.getThreadByIdHandler(request),
+    handler: (request) => handler.getThreadByIdHandler(request),
   },
 ];
 
