0b99cd68e03046520ec42b4a8aae1db6f1c52c61
Vito <vitohartanto@mail.ugm.ac.id>
2024-12-23 14:54:27 +0700

    ReplyRepository.js

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,27 @@
+class ReplyRepository {
+  async addReply(userId, commentId, newReply) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getRepliesByCommentId(commentId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getRepliesByThreadId(threadId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReplyById(id) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async checkReplyAvailability(replyId, commentId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyReplyOwner(id, owner) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ReplyRepository;
